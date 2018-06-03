f345a81da8c340f5923493599643dcc059f1c450
masalism <mantasmasalis@gmail.com>
2018-06-03 13:01:20 +0300

    variables

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+var name = 'Mantas'; //string
+console.log(name);
+
+var lastName = 'Masalis';
+console.log(lastName);
+
+var age = 28;  //number
+console.log(age);
+
+var fullAge = true;  //bool
+console.log(fullAge);
+
+//console.log('Hello world!');
