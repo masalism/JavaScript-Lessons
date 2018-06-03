bf2d19920870c51e2c386b914e211213f4a98909
masalism <mantasmasalis@gmail.com>
2018-06-03 17:12:08 +0300

    objects

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -302,4 +302,69 @@ console.log(jane);
 
 */
 
+////////////////////////////////
+//LECTURE: OBJECTS AND METHODS//
+////////////////////////////////
+
+
+//v1.0
+/*
+var john = {
+    name: 'John', 
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() { //function expression
+        return 2018 - this.yearOfBirth; //this = john
+    }
+};
+
+
+//console.log(john.family[2]);
+//console.log(john.calculateAge(1990));
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+
+console.log(john);
+
+
+//v2.0
+var john = {
+    name: 'John', 
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() { //function expression
+        this.age = 2018 - this.yearOfBirth; //create new variable in john
+    }
+};
+
+john.calculateAge();
+console.log(john);
+
+var mike = {
+    name: 'Mike', 
+    lastName: 'Smith',
+    yearOfBirth: 1950,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function() { //function expression
+        this.age = 2018 - this.yearOfBirth; //create new variable in john
+    }
+};
+
+mike.calculateAge();
+console.log(mike);
+*/
+
+//////////////////
+//LECTURE: LOOPS//
+//////////////////
 
