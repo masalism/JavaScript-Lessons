6107a4fd6defec36d43d745d7bcbd63e0a95b400
masalism <mantasmasalis@gmail.com>
2018-06-03 16:40:06 +0300

    objects

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -265,8 +265,41 @@ if (john.indexOf('teacher') === -1) {
     console.log('John is not a teacher');
 }
 
-*/
-
 ////////////////////
 //LECTURE: OBJECTS//
 ////////////////////
+
+var john = {
+    name: 'John', 
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
+};
+
+//reading values out of object
+console.log(john.lastName); 
+console.log(john['lastName']);
+
+var xyz = 'job'; //inserted job 
+console.log(john[xyz]);
+
+//data mutation
+john.lastName = 'Miller';
+john['job'] = 'programmer';
+
+console.log(john);
+
+var jane = new Object();
+
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane.isMarried = true;
+
+console.log(jane);
+
+*/
+
+
