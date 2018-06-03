fe5bd61f18bcf4be71500e82a23e399b32cb9195
masalism <mantasmasalis@gmail.com>
2018-06-03 13:33:19 +0300

    variables 2

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,3 +1,8 @@
+//////////////////////
+//LECTURE: VARIABLES//
+//////////////////////
+
+/*
 var name = 'Mantas'; //string
 console.log(name);
 
@@ -10,4 +15,34 @@ console.log(age);
 var fullAge = true;  //bool
 console.log(fullAge);
 
-//console.log('Hello world!');
+console.log('Hello world!');
+*/
+
+////////////////////////
+//LECTURE: VARIABLES 2//
+////////////////////////
+
+var name = 'Mantas';
+var age = 28;
+
+//console.log(name + age); //string
+//console.log(age + age); //number
+
+var job, isMarried; //undefined
+
+//console.log(job);
+
+job = 'coder';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'Twenty eight';
+job = 'programmer';
+
+console.log(name + ' is a ' + age + ' years ' + job + '. Is he married? ' + isMarried + '.');
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' years ' + job + '. Is he married? ' + isMarried + '.'); //alert window
