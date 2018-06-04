9504c2fa6bd0264aa1143043334932a898e0f619
masalism <mantasmasalis@gmail.com>
2018-06-04 18:11:32 +0300

    JS work behind the scenes

---
+++ 3-How-JS-Works/script.js
@@ -0,0 +1,77 @@
+//////////////////////
+// LECTURE: HOISTING//
+//////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////
+// LECTURE: SCOPING//
+/////////////////////
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+//////////////////////////
+// LECTURE: THIS KEYWORD//
+//////////////////////////
+
+
+
+
+
+
+
+
