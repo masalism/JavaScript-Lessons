9badeef391dc321630acf69d43cb8c65cb5281bf
masalism <mantasmasalis@gmail.com>
2018-06-06 17:48:08 +0300

    pig game 1

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -8,3 +8,21 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+//variables
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1; //random number 1-6
+
+document.querySelector('#current-' + activePlayer).textContent = dice; //can only select text
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; //selects html
+
+var x = document.querySelector('#score-0').textContent; //reads content
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none'; //changing css
