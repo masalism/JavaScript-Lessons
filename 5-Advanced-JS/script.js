2b6d5e1626ce2cf414143d0b0b75e2e5e117c416
masalism <mantasmasalis@gmail.com>
2018-06-12 20:43:33 +0300

    f

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -30,9 +30,6 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
-*/
-
-
 
 // Object.create //
 
@@ -53,3 +50,149 @@ var jane = Object.create(personProto,
     yearOfBirth: { value: 1969},
     job: { value: 'artist' }
 });
+
+
+
+// Primitives vs objects //
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+//Objects
+var obj1 = {
+    name: 'John',
+    age: 28
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Functions
+var age = 27;
+var obj = {
+    name: 'Mantas',
+    city: 'Kaunas'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'Vilnius';
+}
+
+change(age, obj);
+
+console.log(age); //nesikeicia
+console.log(obj.city); //keiciasi
+
+
+/////////////////////////////////////////////
+// Lecture: Passing functions as arguments //
+/////////////////////////////////////////////
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length ; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+
+//callback functions
+function calculateAge(el) {
+    return 2018 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+
+    if(el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1
+    }
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge)
+var rates = arrayCalc(ages, maxHeartRate);
+
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+
+////////////////////////////////////////////
+// Lecture: functions returning functions //
+////////////////////////////////////////////
+
+function interviewQurstion(job) {
+    if (job === 'designer') {
+        return function(name) { //anonymous function
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject you teach, ' + name +'?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?')
+        }
+    }
+}
+
+var teacherQuestion = interviewQurstion('teacher');
+var designerQuesstion = interviewQurstion('designer');
+
+teacherQuestion('John');
+designerQuesstion ('John');
+designerQuesstion('Jane');
+designerQuesstion('Mark');
+designerQuesstion('Mike');
+
+interviewQurstion('teacher')('Mark');
+
+
+
+
+///////////////////
+// Lecture: IIFE //
+///////////////////
+
+
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game();
+
+
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+//console.log(score);
+
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 -
+    goodLuck);
+})(5);
+
+*/
