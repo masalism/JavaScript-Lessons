e608b02b87e56162fb7fcdac228ba57ce2f92d9d
masalism <mantasmasalis@gmail.com>
2018-06-20 17:52:24 +0300

    Closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -196,3 +196,62 @@ game();
 })(5);
 
 */
+
+///////////////////////
+// Lecture: Closures //
+///////////////////////
+
+function retirement (retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2018 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+//retirement(66)(1990);
+
+//CHALENGE
+
+
+/*
+function interviewQurstion(job) {
+    if (job === 'designer') {
+        return function(name) { //anonymous function
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject you teach, ' + name +'?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?')
+        }
+    }
+}
+*/
+
+function interviewQurstion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject you teach, ' + name +'?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQurstion('teacher')('John');
+interviewQurstion('designer')('Mike');
+interviewQurstion('Builder')('Karius');
