7c88c1927be297979cef4a9460fc0e63c431830b
masalism <mantasmasalis@gmail.com>
2018-06-23 10:19:22 +0300

    i

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -348,6 +348,7 @@ c) correct answer (I would use a number for this)
 */
 
 //safe code
+/*
 (function() {
     //function constructor
     function Question(question, answers, correct) {
@@ -388,5 +389,82 @@ c) correct answer (I would use a number for this)
     var answer = parseInt(prompt('Please select the correct answer.'));
 
     questions[n].checkAnswer(answer);
+})();
+*/
+
+(function() {
+    //function constructor
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function()
+    {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
+        if (ans === this.correct) {
+            console.log('Correct answer');
+            sc = callback(true);
+        } else {
+            console.log('Wrong answer. Try again!');
+            sc = callback(false);
+        }
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current socre is: ' + score);
+        console.log('-----------------------------')
+    }
+
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0); //skaiciukas reiskia teisinga atsakyma
+
+    var q2 = new Question('What is the name of this course\' teacher?', ['John', 'Micheal', 'Jonas'], 2);
+
+    var q3 = new Question('What does best descrive coding?', ['boring', 'Hard', 'Fun', 'Tediuos'], 2); 
+
+    var questions = [q1, q2, q3];
+
+    function score() {
+        var sc = 0;
+        return function (correct) {
+            if(correct) {
+                sc++;
+            }
+            return score;
+        }
+    }
+
+    var keepScore = score();
+
+
+    function nextQuestion() {
+        var n = Math.floor(Math.random() * questions.length); //Math.floor duoda apvalu skaiciu
+
+        questions[n].displayQuestion();
+
+        var answer = prompt('Please select the correct answer.');
+
+        questions[n].checkAnswer(answer);
+
+        if (answer !== 'exit') {
+
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+
+            nextQuestion();
+        }
+       
+    }
+    nextQuestion();
+  
 })();
 
