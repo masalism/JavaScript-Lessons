d523db3bbc1f57ae0b53b04fa8c0a5be291142e6
masalism <mantasmasalis@gmail.com>
2018-06-29 11:02:36 +0300

    reading inout data

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -6,13 +6,42 @@ var budgetController = (function() {
 //UI CONTOROLLER
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    //Public function
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+
+        },
+
+        //Exposed strings to the public
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
+
         // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -24,7 +53,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     //ENTER key functionality
     document.addEventListener('keypress', function(event) {
