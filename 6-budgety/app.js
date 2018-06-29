4e959b7ce852e84ac47af89690af1771bd90a59a
masalism <mantasmasalis@gmail.com>
2018-06-29 10:22:28 +0300

    from start

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -1,66 +1,11 @@
-//BUDGET CONTROLLER
-var budgetController = (function () {
-    
+var budgetController = (function() {
+
 })();
 
-//UI CONTORLLER
 var UIController = (function() {
 
-    //Strings
-    var DOMstrings = {
-        inputType: '.add__type',
-        inputDescription: '.add__description',
-        inputValue: '.add__value',
-        inputBtn: '.add__btn'
-    }
-
-    return {
-        getInput: function() {
-            return {
-                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
-                description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
-            };
-        },
-
-        //Get strings from other controller
-        getDOMstrings: function () {
-            return DOMstrings;
-        }
-    };
-
 })();
 
+var controller = (function () {
 
-//GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
-
-    var DOM = UICtrl.getDOMstrings();
-
-    var ctrlAddItem = function () {
-        // 1. Get the field input data
-        var input = UICtrl.getInput();
-        console.log(input);
-
-        // 2. Add the item to the budget controller
-
-        // 3. Add the item to the UI
-
-        // 4. Calculate the budget
-
-        // 5. Display the budget
-
-    }
-
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    //ENTER key functionality
-    document.addEventListener('keypress', function(event) {
-        
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-
-    });
-
-})(budgetController, UIController);
+})();
