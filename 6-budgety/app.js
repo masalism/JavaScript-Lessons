9090b9f54c71feb49b20f71f722660d4f47322dd
masalism <mantasmasalis@gmail.com>
2018-06-29 10:43:02 +0300

    5

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -1,11 +1,37 @@
+//BUDGET CONTROLLER
 var budgetController = (function() {
 
 })();
 
+//UI CONTOROLLER
 var UIController = (function() {
 
 })();
 
-var controller = (function () {
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
 
-})();
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    //ENTER key functionality
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+
+})(budgetController, UIController);
