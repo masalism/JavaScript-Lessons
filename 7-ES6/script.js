68f15245c07f980549285b3812a51e0c7ac22ed9
masalism <mantasmasalis@gmail.com>
2018-06-30 09:07:07 +0300

    blocks and iifes

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -55,4 +55,28 @@ console.log(i);
 ///////////////////////////////
 // LECTURE: BLOCKS AND IIFES //
 ///////////////////////////////
+/*
+// ES5
+(function () {
+    var c = 3;
+})();
+
+//console.log(c);
+
+// ES6
+// Block scoped, not accesible
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+//console.log(a + b);
+console.log(c);
+*/
+
+//////////////////////
+// LECTURE: STRINGS //
+//////////////////////
+
 
