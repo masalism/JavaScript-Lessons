1ab4e6f83582476fe907163df16dbe54539ccb7a
masalism <mantasmasalis@gmail.com>
2018-06-30 09:34:19 +0300

    arrow functions

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -100,5 +100,33 @@ console.log(n.includes(' '));    //includes
 console.log(`${firstName} `.repeat(5));   //repeating
 */
 
-////////////////////////////////
-// 
+//////////////////////////////
+// LECTURE: ARROW FUNCTIONS //
+//////////////////////////////
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2018 - el;
+});
+
+console.log(ages5);
+
+// ES6
+//arrow function
+let ages6 = years.map(el => 2018 - el); 
+
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2018 - el}.`);
+
+console.log(ages6)
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`;
+});
+
+console.log(ages6);
