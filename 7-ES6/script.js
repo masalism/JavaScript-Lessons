b82c2065cd017c96171331243c65cc209f1029b8
masalism <mantasmasalis@gmail.com>
2018-06-30 09:01:25 +0300

    ES6 variables

---
+++ 7-ES6/script.js
@@ -0,0 +1,58 @@
+////////////////////////////
+// LECTURE: LET AND CONST //
+////////////////////////////
+
+/*
+// ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+
+console.log(name5);
+
+// ES6
+const name6 = 'Jane Smith'; //const - unchangeable variable
+let age6 = 23;  //let - changeable variable
+name6 = 'Jane Miller';
+console.log(name6);
+
+// ES5
+function driverLicense5(passedTest) {
+    if (passedTest) {
+        console.log(firstName);
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+
+    console.log(firstName + ' born in ' + yearOfBirth + ' is now officialy allowed to drive a car');
+}
+
+driverLicense5(true);
+
+// ES6
+function driverLicense6(passedTest) {
+    let firstName;
+    const yearOfBirth = 1990;
+
+    if (passedTest) {
+        firstName = 'John';        
+    }
+
+    console.log(firstName + ' born in ' + yearOfBirth + ' is now officialy allowed to drive a car');
+}
+
+driverLicense6(true);
+
+let i = 23;
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
+*/
+
+///////////////////////////////
+// LECTURE: BLOCKS AND IIFES //
+///////////////////////////////
+
