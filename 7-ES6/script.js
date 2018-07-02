935906329b24e77ae370743232864c9b92f48161
masalism <mantasmasalis@gmail.com>
2018-07-02 14:48:15 +0300

    rest parameters

--- 7-ES6/script.js
+++ 7-ES6/script.js
@@ -135,6 +135,7 @@ console.log(ages6);
 // LECTURE: ARROW FUNCTIONS 2 //
 ////////////////////////////////
 
+/*
 // ES5
 var box5 = {
     color: 'green',
@@ -208,3 +209,193 @@ Person.prototype.myFriends6 = function(friends) {
 }
 
 new Person('Mike').myFriends6(friends);
+*/
+
+////////////////////////////
+// LECTURE: DESTRUCTURING //
+////////////////////////////
+
+/*
+// ES5
+var john = ['John', 26];
+//var name = john[0];
+//var age = john[1];
+
+// ES6
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+
+console.log(firstName, lastName);
+
+
+const {firstName: a, lastName: b} = obj;
+
+console.log(a);
+console.log(b);
+
+
+
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
+*/
+
+/////////////////////
+// LECTURE: ARRAYS //
+/////////////////////
+
+/*
+const boxes = document.querySelectorAll('.box');
+
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor = 'dodgerblue';
+});
+
+
+// ES6
+
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach( cur => cur.style.backgroundColor = 'dodgerblue');
+
+
+// ES5
+/* 
+for (var i = 0; i < boxesArr5.length; i++) {
+
+    if(boxesArr5[i].className === 'box blue') {
+        break;
+    }
+
+    boxesArr5[i].textContent = 'I changed to blue!';
+}
+*/
+
+// ES6
+/*
+for (const cur of boxesArr6) {
+    if(cur.className.includes('blue')) {
+        continue;
+    }
+    cur.textContent = 'I changed to blue!';
+}
+
+// ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function(cur) {
+    return cur >= 18;
+});
+
+console.log(full);
+
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+// ES6
+console.log(ages.findIndex(cur => cur >= 18));
+
+console.log(ages.find(cur => cur >= 18));
+
+*/
+
+//////////////////////////////
+// LECTURE: SPREAD OPERATOR //
+//////////////////////////////
+
+/*
+function addFourAges(a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+
+console.log(sum1);
+
+// ES5
+
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+
+console.log(sum2);
+
+// ES6
+
+const sum3 = addFourAges(...ages);
+
+console.log(sum3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+
+const familyMiller = ['Mary', 'Bob', 'Ann', 'Mantas'];
+
+const bigFamily = [...familySmith, 'Lilly', ...familyMiller];
+
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
+*/
+
+//////////////////////////////
+// LECTURE: REST PARAMETERS //
+//////////////////////////////
+
+/*
+// ES5
+function isFullAge5() {
+   // console.log(arguments);
+   var argsArr = Array.prototype.slice.call(arguments);
+
+   argsArr.forEach(function(cur) {
+       console.log((2016 - cur) >= 18);
+   });
+}
+//isFullAge5(1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2011, 1997)
+
+// ES6
+function isFullAge6(...years) {
+    years.forEach(cur => console.log((2018 - cur) >= 18));
+}
+
+isFullAge6(1990, 1999, 1965, 2011);
+*/
+
+// ES5
+function isFullAge5(limit) {
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+    console.log(argsArr);
+
+    argsArr.forEach(function(cur) {
+        console.log((2016 - cur) >= limit);
+    });
+ }
+ isFullAge5(18, 1990, 1999, 1965);
+ //isFullAge5(1990, 1999, 1965, 2011, 1997)
+ 
+ // ES6
+ function isFullAge6(limit, ...years) {
+     years.forEach(cur => console.log((2018 - cur) >= limit));
+ }
+ 
+ isFullAge6(16, 1990, 1999, 1965, 2011);
